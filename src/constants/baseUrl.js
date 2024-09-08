3243a3c28e5c43e90e955249edcf850358f5a865
gagan2125 <gagansk2125@gmail.com>
2024-09-08 22:04:20 +0530

    cat and sub cat update 1

--- src/constants/baseUrl.js
+++ src/constants/baseUrl.js
@@ -1,3 +1,3 @@
-//var url = "https://evol8-department-backend.vercel.app";
-var url = "http://localhost:5000";
+var url = "https://evol8-department-backend.vercel.app";
+//var url = "http://localhost:5000";
 export default url;
